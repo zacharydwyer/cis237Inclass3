82af022d28cd24b9cf7f97a366e59a7c17cb0d18
David Barnes <dbarnes@kvcc.edu>
2015-09-28 20:13:25 -0400

    Add Employee class.

---
+++ cis237Inclass3/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass3
+{
+    class Employee
+    {
+        //Private variables
+        private string firstName;
+        private string lastName;
+        private string department;
+
+        //Propertiest for variables
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        //3 Parameter Constructor
+        public Employee(string FirstName, string LastName, string Department)
+        {
+            this.firstName = FirstName;
+            this.lastName = LastName;
+            this.department = Department;
+        }
+
+
+    }
+}
